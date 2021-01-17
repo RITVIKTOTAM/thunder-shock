be92e4b9050e85d5697887e777b76f4d563231e0
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2021-01-17 09:07:30 +0530

    Update boy.js

--- boy.js
+++ boy.js
@@ -8,7 +8,7 @@ class Boy{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.animation = loadAnimation("proc41images/images/Walking Frame/walking_1.png","proc41images/images/Walking Frame/walking_2.png","proc41images/images/Walking Frame/walking_3.png","proc41images/images/Walking Frame/walking_4.png","proc41images/images/Walking Frame/walking_5.png","proc41images/images/Walking Frame/walking_6.png","proc41images/images/Walking Frame/walking_7.png","proc41images/images/Walking Frame/walking_8.png");
+        this.animation = loadAnimation("walking_1.png","walking_2.png","walking_3.png","walking_4.png","walking_5.png","walking_6.png","walking_7.png","walking_8.png");
         World.add(world, this.body);
       }
       display(){
